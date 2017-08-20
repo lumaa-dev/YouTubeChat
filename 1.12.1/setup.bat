f2f0845dbfd79269c02c9a9188f79d7ba4d71351
SteveKunG <mccommander_minecraft@hotmail.com>
2017-08-20 20:07:34 +0700

    -.-

--- 1.12.1/setup.bat
+++ 1.12.1/setup.bat
@@ -1,2 +1,2 @@
-set GRADLE_USER_HOME=M:\Minecraft Modding Workspace\youtube-chat-for-minecraft\1.12.1\.gradle_data
+set GRADLE_USER_HOME=M:\Modding\StreamChat\1.12.1\.gradle_data
 gradlew setupDecompWorkspace eclipse
--- 1.8.9/build.bat
+++ 1.8.9/build.bat
@@ -1,2 +1,2 @@
-set GRADLE_USER_HOME=M:\Minecraft Modding Workspace\youtube-chat-for-minecraft\1.8.9\.gradle_data
+set GRADLE_USER_HOME=M:\Modding\StreamChat\1.8.9\.gradle_data
 gradlew build
