c1e58231f8ea3589f0aa0a1befe641e3a0522162
SteveKunG <mccommander_minecraft@hotmail.com>
2018-09-12 16:40:29 +0700

    Update workspace

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
-rd /s /q "M:\Modding\YouTubeChat\1.12.2\build\classes"
-rd /s /q "M:\Modding\YouTubeChat\1.12.2\build\dependency-cache"
-rd /s /q "M:\Modding\YouTubeChat\1.12.2\build\libs"
-rd /s /q "M:\Modding\YouTubeChat\1.12.2\build\resources"
-rd /s /q "M:\Modding\YouTubeChat\1.12.2\build\sources"
-set GRADLE_USER_HOME=M:\Modding\YouTubeChat\1.12.2\.gradle_data
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\classes"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\dependency-cache"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\libs"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\resources"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\sources"
+set GRADLE_USER_HOME=M:\Modding\YouTubeChat\1.13-pre\.gradle_data
 gradlew build
