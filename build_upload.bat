018070c46351b6234206a7605293d00f10929ab1
SteveKunG <mccommander_minecraft@hotmail.com>
2018-09-18 14:29:41 +0700

    Update workspace

---
+++ build_upload.bat
@@ -0,0 +1,7 @@
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\classes"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\dependency-cache"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\libs"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\resources"
+rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\sources"
+set GRADLE_USER_HOME=M:\Modding\YouTubeChat\1.13-pre\.gradle_data
+gradlew buildJar curseforge
---
+++ upload.bat
@@ -0,0 +1,2 @@
+set GRADLE_USER_HOME=M:\Modding\YouTubeChat\1.13-pre\.gradle_data
+gradlew curseforge
