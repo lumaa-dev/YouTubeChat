d4d9953c64427460069b1698fd7bf3c424d25558
SteveKunG <mccommander_minecraft@hotmail.com>
2018-11-20 22:03:29 +0700

    Bump to 1.4.1

--- build_upload.bat
+++ build_upload.bat
@@ -4,4 +4,4 @@ rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\libs"
 rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\resources"
 rd /s /q "M:\Modding\YouTubeChat\1.13-pre\build\sources"
 set GRADLE_USER_HOME=M:\Modding\YouTubeChat\1.13-pre\.gradle_data
-gradlew buildJar curseforge
+gradlew build curseforge
